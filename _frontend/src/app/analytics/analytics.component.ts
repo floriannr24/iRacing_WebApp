dd7167c61e3b8468752bc5e794bee8bc2bbf5d36
Florian Niedermeier <florian-niedermeier@gmx.de>
2024-03-18 20:50:40 +0100

    added: blur content of "Analytics" if no account user info available

--- _frontend/src/app/analytics/analytics.component.ts
+++ _frontend/src/app/analytics/analytics.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from "../_services/data.service";
+import {Account} from "../settings/settings.component";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
+import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
 
 @Component({
   selector: 'app-analytics',
@@ -8,6 +12,11 @@ import { Component } from '@angular/core';
 export class AnalyticsComponent {
   title = "angular-app";
   _showRaceSelectorPanel = false;
+  mainAccount: Account | undefined
+
+  constructor(private dataService: DataService) {
+    this.dataService.mainAcc.pipe(takeUntilDestroyed()).subscribe(acc => this.mainAccount = acc)
+  }
 
   showRaceSelectorPanel() {
     this._showRaceSelectorPanel = !this._showRaceSelectorPanel
